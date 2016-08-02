0184970a566bea7356f7f28ac6fbb321d36433fd
Mark Homan <markzi@gmail.com>
2016-08-02 21:05:46 +0100

    project initial setup

---
+++ src/main/java/uk/co/mjhoman/hystrix/demo/HystrixDemoWebappApplication.java
@@ -0,0 +1,12 @@
+package uk.co.mjhoman.hystrix.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixDemoWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDemoWebappApplication.class, args);
+	}
+}
