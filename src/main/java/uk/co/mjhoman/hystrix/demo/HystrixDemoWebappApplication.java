14a2cc16026ad1d614e5fcfb09ac27c303674a30
Mark Homan <markzi@gmail.com>
2016-08-02 22:51:23 +0100

    simple working example

--- src/main/java/uk/co/mjhoman/hystrix/demo/HystrixDemoWebappApplication.java
+++ src/main/java/uk/co/mjhoman/hystrix/demo/HystrixDemoWebappApplication.java
@@ -2,8 +2,12 @@ package uk.co.mjhoman.hystrix.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class HystrixDemoWebappApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/uk/co/mjhoman/hystrix/demo/web/spring/controller/HystrixController.java
@@ -0,0 +1,33 @@
+package uk.co.mjhoman.hystrix.demo.web.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import uk.co.mjhoman.hystrix.demo.command.HystrixDemoCommand;
+
+@Controller
+public class HystrixController {
+
+	@Autowired
+	private HystrixDemoCommand command;
+
+	@RequestMapping("/")
+	public String root() {
+		return "forward:/hystrix";
+	}
+
+	@RequestMapping("/request")
+	public String request(@RequestParam("mode") String mode) {
+
+		try {
+			command.request(mode);
+		} catch (IllegalArgumentException illegalArgumentException) {
+			// do nothing
+		}
+
+		return "index";
+	}
+
+}
